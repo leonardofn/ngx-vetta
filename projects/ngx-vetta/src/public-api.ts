44471cfef1666bfa6e814143122948d804d6db7b
Leonardo Nogueira <leonardo.nogueira@vetta.digital>
2023-05-06 14:36:17 -0300

    feat(ngx-vetta-lib): adicionando scripts de build

--- projects/ngx-vetta/src/public-api.ts
+++ projects/ngx-vetta/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of ngx-vetta
  */
 
-export * from './lib/ngx-vetta.service';
 export * from './lib/ngx-vetta.component';
 export * from './lib/ngx-vetta.module';
+export * from './lib/ngx-vetta.service';
