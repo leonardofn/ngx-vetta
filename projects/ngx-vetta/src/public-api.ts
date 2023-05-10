1dff5eb3927e382705642aae4e7bdd04989b7db5
Leonardo Nogueira <leonardo.nogueira@vetta.digital>
2023-05-10 19:39:34 -0300

    chore: exportando diretiva decimal-number-mask

--- projects/ngx-vetta/src/public-api.ts
+++ projects/ngx-vetta/src/public-api.ts
@@ -4,5 +4,7 @@
 
 export * from './lib/components/test/test.component';
 export * from './lib/components/test/test.module';
+export * from './lib/directives/decimal-number-mask/decimal-number-mask.directive';
+export * from './lib/directives/decimal-number-mask/decimal-number-mask.module';
 export * from './lib/directives/validation-label/validation-label.directive';
 export * from './lib/directives/validation-label/validation-label.module';
