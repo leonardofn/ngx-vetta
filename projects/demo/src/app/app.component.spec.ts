9b08c507f6596f431bc93a6a125a4932a4f0ceaa
Leonardo Nogueira <leonardo.nogueira@vetta.digital>
2023-05-19 21:32:52 -0300

    fix: corrigir testes do app de demo

--- projects/demo/src/app/app.component.spec.ts
+++ projects/demo/src/app/app.component.spec.ts
@@ -14,18 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'demo'`, () => {
+  it(`should have as title 'Ngx Vetta'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('demo');
+    expect(app.title).toEqual('Ngx Vetta');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain(
-      'demo app is running!'
-    );
+    expect(compiled.querySelector('.form-box h3').textContent).toContain('NgxVetta Demo');
   });
 });
